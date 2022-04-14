b883a012db5fb9eb3567c3a3f122170574219955
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-04-14 18:34:33 +0100

    Create function for computer player
    The function Randomly return either Rock, Paper or Scissors

---
+++ script.js
@@ -0,0 +1,9 @@
+// Randomly return either Rock, Paper or Scissors
+function computerPlay() {
+    let choice;
+    random = Math.floor(Math.random() * 3) + 1
+    random === 1 ? choice = "Rock" :
+        random === 2 ? choice = "Paper" :
+            choice = "Scissors";
+    return choice;
+}
