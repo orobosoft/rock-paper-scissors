dd4e1daefac18c4c3837bb3d096c212dabaf717e
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-05-02 14:09:02 +0100

    Create functionality to start the game
    Add event listener to the play game button to enter the game section
    Create variables to select the UI elements

--- script.js
+++ script.js
@@ -41,3 +41,25 @@ function playRound(playerSelection, computerSelection) {
 	}
 	return result;
 }
+
+// Go to the game section
+function startGame() {
+	document.querySelector(".intro").style.display = "none";
+	document.querySelector(".gameplay").style.display = "block";
+}
+
+// Score variables
+let playerScore = 0;
+let draw = 0;
+let computerScore = 0;
+
+// UI selection variables
+const playerScoreUi = document.querySelector("#player-score span");
+const drawUi = document.querySelector("#draw-score span");
+const computerScoreUi = document.querySelector("#computer-score span");
+const options = document.querySelectorAll(".options button");
+const start = document.querySelector("#playgame-btn");
+const xBtn = document.querySelector(".x-btn");
+
+// Event Listeners
+start.addEventListener("click", startGame);
